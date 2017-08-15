a222a103ac71518d60455439d4476000e8ebb41d
rroy <roy.rajib@live.com>
2017-08-15 13:13:14 -0600

    PisoFoam for Taylor Green Vortex error norm 2

---
+++ TGPisoFoam.C
@@ -0,0 +1,159 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    myPisoFoam // modified to comply with adjustableTimeStep
+
+Description
+    Transient solver for incompressible flow.
+
+    Turbulence modelling is generic, i.e. laminar, RAS or LES may be selected.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "singlePhaseTransportModel.H"
+#include "turbulenceModel.H"
+#include "IFstream.H"
+#include "OFstream.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createFields.H"
+    #include "initContinuityErrs.H"
+    # include "readTimeControls.H" 
+    
+    #include "readTransportProperties.H"
+    #include "createOutFile.H" // Create output variables and outfile
+    outFile << "Time\t" << "Ek\t" << "epsilon\t" << "L2(U)\t" << "L2(p)" << endl;
+    
+    // Calculate analytical field at time t=0;
+    #include "TGFieldAtTime0.H"
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        #include "readPISOControls.H"
+        #include "CourantNo.H"
+	# include "setDeltaT.H" 
+
+        // Pressure-velocity PISO corrector
+        {
+            // Momentum predictor
+
+            fvVectorMatrix UEqn
+            (
+                fvm::ddt(U)
+              + fvm::div(phi, U)
+              + turbulence->divDevReff(U)
+            );
+
+            UEqn.relax();
+
+            if (momentumPredictor)
+            {
+                solve(UEqn == -fvc::grad(p));
+            }
+
+            // --- PISO loop
+
+            for (int corr=0; corr<nCorr; corr++)
+            {
+                volScalarField rAU(1.0/UEqn.A());
+
+                U = rAU*UEqn.H();
+                phi = (fvc::interpolate(U) & mesh.Sf())
+                    + fvc::ddtPhiCorr(rAU, U, phi);
+
+                adjustPhi(phi, U, p);
+
+                // Non-orthogonal pressure corrector loop
+                for (int nonOrth=0; nonOrth<=nNonOrthCorr; nonOrth++)
+                {
+                    // Pressure corrector
+
+                    fvScalarMatrix pEqn
+                    (
+                        fvm::laplacian(rAU, p) == fvc::div(phi)
+                    );
+
+                    pEqn.setReference(pRefCell, pRefValue);
+
+                    if
+                    (
+                        corr == nCorr-1
+                     && nonOrth == nNonOrthCorr
+                    )
+                    {
+                        pEqn.solve(mesh.solver("pFinal"));
+                    }
+                    else
+                    {
+                        pEqn.solve();
+                    }
+
+                    if (nonOrth == nNonOrthCorr)
+                    {
+                        phi -= pEqn.flux();
+                    }
+                }
+
+                #include "continuityErrs.H"
+
+                U -= rAU*fvc::grad(p);
+                U.correctBoundaryConditions();                
+            }
+        }        
+        turbulence->correct();
+
+        runTime.write();
+        
+                
+        // Calculate error norms L2
+        #include "errorNorm.H"
+        
+        // Calculate global properties
+        #include "globalProperties.H"
+        #include "writeOutput.H"
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+// ************************************************************************* //
